71daf8b5ff85a9fa2ad25004ea31247ce630c8b3
srija4444 <srijaanitha2022@gmail.com>
2025-09-12 14:19:13 +0530

    Update student_db.py

--- student_db.py
+++ student_db.py
@@ -41,9 +41,10 @@ def add_student(student):
     # Check if StudentID already exists in the database
     cursor.execute("SELECT * FROM Students WHERE StudentID = ?", (student[0],))
     if cursor.fetchone():
-        print("❌ Student ID already exists!")
+        print(" Student ID already exists!")
     else:
         # Proceed with the insert if the StudentID is unique
         cursor.execute("INSERT INTO Students (StudentID, FirstName, LastName, Age, Grade, Email) VALUES (?, ?, ?, ?, ?, ?)", student)
         conn.commit()
-        print("✅ Student added!")
+        print(" Student added!")
+
